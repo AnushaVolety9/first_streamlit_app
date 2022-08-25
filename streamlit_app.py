9b6bf89b6fd7b3bdab2ea477117918a2e5fd23ff
Anusha Volety <112089867+AnushaVolety9@users.noreply.github.com>
2022-08-25 18:30:01 +0530

    Update streamlit_app.py
    Added menu to family menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Dad see im growing :D')
+streamlit.title('House Menu')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
